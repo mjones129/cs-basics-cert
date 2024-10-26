515753eca5f70b6e48497ec67d2a702b7a12704b
Matt Jones <me@mattjones.tech>
2024-10-26 17:21:40 -0400

    multiline comment

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-﻿string firstName = "Bob";
+﻿/*
+string firstName = "Bob";
 int widgetsPurchased = 7;
-// Testing a change to the message.
-// int widgetsSold = 7;
-// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
 Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+*/
