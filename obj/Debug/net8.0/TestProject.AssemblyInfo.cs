248c7893325f0dd3c9b58521f711434e0f5eddfa
Matt Jones <me@mattjones.tech>
2024-10-26 17:27:53 -0400

    low quality comments

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5e883a53043fd55d78498fce471e51e398f4320")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+515753eca5f70b6e48497ec67d2a702b7a12704b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
