3150f1fe57b540f4edb60f975958c4c445c0e9e9
Matt Jones <me@mattjones.tech>
2024-10-29 16:45:02 -0400

    login logic

--- Program.cs
+++ Program.cs
@@ -1,8 +1,31 @@
 using System;
+using System.Xml;
 
-int coinflip = new Random().Next(0, 2);
-string result = "";
-if (coinflip == 0)
-result = "heads";
-else result = "tails";
-Console.WriteLine(result);
+//must-use variables
+string permission = "Admin|Manager";
+int level = 19;
+
+string output = "";
+
+if (level > 55 && permission.Contains("Admin"))
+{
+    output = "Welcome, Super Admin user.";
+}
+else if (level >= 55 && permission.Contains("Admin"))
+{
+    output = "Welcome, Admin user.";
+}
+else if (level >= 20 && permission.Contains("Manager"))
+{
+    output = "Contact an Admin for access.";
+}
+else if (level < 20 && permission.Contains("Manager"))
+{
+    output = "You do not have sufficient priveleges.";
+}
+else
+{
+    output = "You do not have sufficient privileges.";
+}
+
+Console.WriteLine(output);
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df92fcc5537d80754d404cd790fd0e280ae99607")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bd5b597e2883933ae2bc0ba786b6ff250856106")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
