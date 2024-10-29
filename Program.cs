8bd5b597e2883933ae2bc0ba786b6ff250856106
Matt Jones <me@mattjones.tech>
2024-10-29 16:15:07 -0400

    coin flip

--- Program.cs
+++ Program.cs
@@ -1,30 +1,8 @@
-// string value1 = " a";
-// string value2 = "A ";
-// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+using System;
 
-// Console.WriteLine("a" != "a");
-// Console.WriteLine("a" != "A");
-// Console.WriteLine(1 != 2);
-
-// string myValue = "a";
-// Console.WriteLine(myValue != "a");
-
-// Console.WriteLine(1 > 2);
-// Console.WriteLine(1 < 2);
-// Console.WriteLine(1 >= 1);
-// Console.WriteLine(1 <= 1);
-
-// string pangram = "The quick brown fox jumps over the lazy dog.";
-// Console.WriteLine(pangram.Contains("fox"));
-// Console.WriteLine(pangram.Contains("cow"));
-
-// string pangram = "The quick brown fox jumps over the lazy dog.";
-// Console.WriteLine(!pangram.Contains("fox"));
-// Console.WriteLine(!pangram.Contains("cow"));
-
-int a = 7;
-int b = 6;
-Console.WriteLine(a != b); //output: True
-string s1 = "Hello";
-string s2 = "Hello";
-Console.WriteLine(s1 != s2); //output: False
+int coinflip = new Random().Next(0, 2);
+string result = "";
+if (coinflip == 0)
+result = "heads";
+else result = "tails";
+Console.WriteLine(result);
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913944740da6a787d294bc5f76c6857cd27de550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df92fcc5537d80754d404cd790fd0e280ae99607")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
