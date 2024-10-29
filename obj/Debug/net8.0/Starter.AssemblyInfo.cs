bd2848810609c8cbdfa4ae6dc9e8c4006a490e67
Matt Jones <me@mattjones.tech>
2024-10-29 14:18:31 -0400

    confirming input to ignore case sensativity in user input

--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17f1dba64023aa867da5fc704a2706174c36b0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e936a0b943c24ce5ab3f50eb33f5b6bcce2ecd02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
