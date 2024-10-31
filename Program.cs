cb4cf5c61185b8ccd2988bae27c29f2e7465b8bf
Matt Jones <me@mattjones.tech>
2024-10-31 19:59:52 -0400

    do while game challenge

--- Program.cs
+++ Program.cs
@@ -1,19 +1,33 @@
 using System;
 
 Random random = new Random();
-int current = random.Next(1, 11);
+int heroHealth = 10;
+int monsterHealth = 10;
+// int attack = random.Next(1, 10);
+// int turn = 0;
 
+
+// HERO'S TURN
 do
 {
-    current = random.Next(1, 11);
-    if (current >= 8) continue;
-    
-    Console.WriteLine(current);
-} while (current != 7);
-
-// while (current >= 3)
-// {
-//     Console.WriteLine(current);
-//     current = random.Next(1, 11);
-// }
-// Console.WriteLine($"Last number: {current}");
+    int heroAttack = random.Next(1, 10);
+    if (monsterHealth > 0)
+    {
+        monsterHealth = monsterHealth - heroAttack;
+        Console.WriteLine($"Monster was damaged and lost {heroAttack} health and now has {monsterHealth} health.");
+    }
+    else
+    {
+        Console.WriteLine("Hero wins!");
+    }
+    int monsterAttack = random.Next(1, 10);
+    if (heroHealth > 0)
+    {
+        heroHealth = heroHealth - monsterAttack;
+        Console.WriteLine($"Hero was damaged and lost {monsterAttack} health and now has {heroHealth} health.");
+    }
+    else
+    {
+        Console.WriteLine("Monster wins!");
+    }
+} while (monsterHealth > 0 || heroHealth > 0);
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9fc491101f41a2d50ba325d8ae627352e30abbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438e57a365d0a892a7c36cf79594b75b5814ccc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
