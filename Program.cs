0ab54277fd5f0d6f3e33533fc68003ec24dd2ba0
Matt Jones <me@mattjones.tech>
2024-10-31 14:31:20 -0400

    fizz buzz solution

--- Program.cs
+++ Program.cs
@@ -2,18 +2,28 @@ using System;
 using System.Runtime.ExceptionServices;
 using System.Xml;
 
-//foreach loop limitation resolved:
+//Fizz Buzz in C# LET'S GOOOOOOOO
+// output values 1 to 100, one number per line
+// when the current value is divisible by 3, print `Fizz` next to the number
+// when the current value is divisible by 5, print `Buzz next to the number
+// when the current value is divisible by both 3 and 5, print the term `FizzBuzz` next to the number
 
-string[] names = { "Alex", "Eddie", "David", "Michael" };
-for (int i = 0; i < names.Length; i++)
+for (int i = 1; i <= 100; i++)
 {
-    if (names[i] == "David")
+    if (i % 3 == 0 && i % 5 == 0)
     {
-        names[i] = "Sammy";
+        Console.WriteLine($"{i} - FizzBuzz");
     }
-}
-
-foreach (var name in names)
-{
-    Console.WriteLine(name);
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} - Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
 }
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be0fc995221a2d8c8ba57beb04e6846c7a0fdd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7e7fddf4f3d6eb1b3bc0377354e81ffa2e9213")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
