2dfebad505222069799fbde53dc7243cb9412a16
Matt Jones <me@mattjones.tech>
2024-10-31 14:09:48 -0400

    alternate for loop method, reversing order

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@ using System;
 using System.Runtime.ExceptionServices;
 using System.Xml;
 
-//using a for loop to iterate through an array
+//using a for loop to iterate through an array (reversed order from the first example, but this order refelcts the natural order in the array as it's initiated)
 
 string[] names = { "Alex", "Eddie", "David", "Michael" };
-for (int i = names.Length - 1; i >= 0; i--)
+for (int i = 0; i < names.Length; i++)
 {
     Console.WriteLine(names[i]);
 }
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5664d3f78420b569b4a636510092884f5ac72056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41fc18343634ce3655ec8bad4944d7b6f28f5ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
