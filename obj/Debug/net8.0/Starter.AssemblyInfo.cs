cb195766e865a2c9cec111688895f39a9b8f87bf
Matt Jones <me@mattjones.tech>
2024-10-31 14:04:16 -0400

    for loop, odd iteration

--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a522c2b08eab3f79a7bde63822ca0e8f3c96f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16c348fa9182d6190ee01e4a276e4b955a0da19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
