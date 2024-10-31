d1a522c2b08eab3f79a7bde63822ca0e8f3c96f8
Matt Jones <me@mattjones.tech>
2024-10-31 14:02:22 -0400

    basic for loop, additive

--- Program.cs
+++ Program.cs
@@ -1,69 +1,7 @@
 using System;
 using System.Xml;
 
-// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
-
-string[] product = sku.Split('-');
-
-string type = "";
-string color = "";
-string size = "";
-
-switch (product[0])
+for (int i = 0; i < 10; i++)
 {
-    case "01":
-    type = "Sweat shirt";
-    break;
-
-    case "02":
-    type = "T-Shirt";
-    break;
-
-    case "03":
-    type = "Sweat Pants";
-    break;
-
-    default:
-    type = "Other";
-    break;
+    Console.WriteLine(i);
 }
-
-switch (product[1])
-{
-    case "BL":
-    color = "Black";
-    break;
-
-    case "MN":
-    color = "Maroon";
-    break;
-
-    default:
-    color = "White";
-    break;
-}
-
-
-switch (product[2])
-{
-    case "S":
-    size = "Small";
-    break;
-
-    case "M":
-    size = "Medium";
-    break;
-
-    case "L":
-    size = "Large";
-    break;
-
-    default:
-    size = "One Size Fits All";
-    break;    
-}
-
-
-Console.WriteLine($"Product: {size} {color} {type}");
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd3e2b594f5bd3abd72e3542c10a746eec20d50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbab9a31821018ce8ef77c72ad79fc820b6c9005")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
