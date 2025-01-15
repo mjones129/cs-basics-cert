e1e8f726cb2ac85a03e7ee3684ba8e32b1e3339d
Matt <me@mattjones.tech>
2025-01-14 20:43:04 -0500

    dotnet build, test for errors

--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77e221409d247185fc40ba19b40f87e0b9615da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b986ecdc28858b2771026ab563f05b8dd044bc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
